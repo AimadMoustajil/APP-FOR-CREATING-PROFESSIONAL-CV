f85e9cfd0994944c9dd4bce44a2c4740da3661fc
Aimad <imadmostajil@gmail.com>
2024-05-28 17:28:36 +0100

    handling splash Screen

---
+++ app/src/main/java/com/example/app_creat_profesionell_cv/LoginAndCreatAccount/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.app_creat_profesionell_cv.LoginAndCreatAccount;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.app_creat_profesionell_cv.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
