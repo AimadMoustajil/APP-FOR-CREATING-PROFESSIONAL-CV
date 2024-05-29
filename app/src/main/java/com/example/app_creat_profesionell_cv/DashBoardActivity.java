6f9af629efbd1a2194ad3a9b0da41f33753d2c97
Aimad <imadmostajil@gmail.com>
2024-05-29 16:48:31 +0100

    creat drawer header

--- app/src/main/java/com/example/app_creat_profesionell_cv/DashBoardActivity.java
+++ app/src/main/java/com/example/app_creat_profesionell_cv/DashBoardActivity.java
@@ -1,14 +1,66 @@
 package com.example.app_creat_profesionell_cv;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.example.app_creat_profesionell_cv.LoginAndCreatAccount.LoginActivity;
+import com.google.android.material.navigation.NavigationView;
 
 public class DashBoardActivity extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
+    ImageButton buttonDrawelToggle;
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+        drawerLayout = findViewById(R.id.dr);
+        buttonDrawelToggle = findViewById(R.id.buttonDrawelToggle);
+        navigationView = findViewById(R.id.navigationView);
+
+        buttonDrawelToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.open();
+            }
+        });
+
+        //header View
+        View view = navigationView.getHeaderView(0);
+        ImageButton imageOfuse = view.findViewById(R.id.imageOfuser);
+        TextView nameOfUser = view.findViewById(R.id.nameOfUser);
+        TextView emailOfUser = view.findViewById(R.id.emailOfUser);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.home){
+
+                } else if (itemId == R.id.about) {
+                    startActivity(new Intent(DashBoardActivity.this,AboutActivity.class));
+                }else if (itemId == R.id.logout){
+                    startActivity(new Intent(DashBoardActivity.this, LoginActivity.class));
+                }
+                return false;
+            }
+        });
+
     }
+
 }
