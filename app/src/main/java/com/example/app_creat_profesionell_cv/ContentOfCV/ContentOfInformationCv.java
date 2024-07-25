d9bd9c02507a76c67137e80b81e0d69866d5fca2
Aimad <imadmostajil@gmail.com>
2024-07-25 21:56:40 +0100

    handling info personnel of user

--- app/src/main/java/com/example/app_creat_profesionell_cv/ContentOfCV/ContentOfInformationCv.java
+++ app/src/main/java/com/example/app_creat_profesionell_cv/ContentOfCV/ContentOfInformationCv.java
@@ -6,22 +6,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.app_creat_profesionell_cv.R;
+import com.example.app_creat_profesionell_cv.SectionOfCV.InfoPersonnelleActivity;
 
 public class ContentOfInformationCv extends AppCompatActivity {
 
-    Button generateCv;
+    TextView infoPersonnel,éducation,exDeTravaille,projet,infoAdditionnelle;
+    Button generateCV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content_of_information_cv);
-        generateCv = findViewById(R.id.generateCv);
-        generateCv.setOnClickListener(new View.OnClickListener() {
+        infoPersonnel = findViewById(R.id.infoPersonnel);
+        éducation = findViewById(R.id.éducation);
+        exDeTravaille = findViewById(R.id.exDeTravaille);
+        projet = findViewById(R.id.projet);
+        infoAdditionnelle = findViewById(R.id.infoAdditionnelle);
+        generateCV = findViewById(R.id.generateCV);
+
+
+        infoPersonnel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(ContentOfInformationCv.this, PDFCV.class));
+                startActivity(new Intent(ContentOfInformationCv.this, InfoPersonnelleActivity.class));
             }
         });
+
     }
 }
--- app/src/main/java/com/example/app_creat_profesionell_cv/SectionOfCV/InfoPersonnelleActivity.java
+++ app/src/main/java/com/example/app_creat_profesionell_cv/SectionOfCV/InfoPersonnelleActivity.java
@@ -3,14 +3,29 @@ package com.example.app_creat_profesionell_cv.SectionOfCV;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.app_creat_profesionell_cv.R;
 
 public class InfoPersonnelleActivity extends AppCompatActivity {
 
+    ImageView imageOfUser,check;
+    EditText firstName,lastName,numberPhone,emailOfUSer,paysOfUser,villeOfUSer,jobOfUSer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_personnelle);
+
+        imageOfUser = findViewById(R.id.imageView3);
+        check = findViewById(R.id.check);
+        firstName = findViewById(R.id.firstName);
+        lastName = findViewById(R.id.lastName);
+        numberPhone = findViewById(R.id.numberPhone);
+        emailOfUSer = findViewById(R.id.emailOfUSer);
+        paysOfUser = findViewById(R.id.paysOfUser);
+        villeOfUSer = findViewById(R.id.villeOfUSer);
+        jobOfUSer = findViewById(R.id.jobOfUSer);
     }
 }
